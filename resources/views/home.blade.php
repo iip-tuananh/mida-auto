36df48d08f130295287e6619970add5ce77ce778
Duongatun19 <95019544+Duongatun19@users.noreply.github.com>
2023-03-09 17:02:15 +0700

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -931,7 +931,7 @@
                   @if (count($cate->typeCate) > 0)
                   <div class="list_link_pr d-flex pt-2 pb-2">
                      @foreach ($cate->typeCate as $type)
-                        <a class=" box-shadow a-style bg-do border rounded-10 font-weight-bold js-tab-title" href="{{route('allListProType', ['danhmuc'=>$type->cate_slug, 'loaidanhmuc'=>$type->slug])}}" title="{{languageName($type->name)}}">
+                        <a class=" button-85 box-shadow a-style bg-do border rounded-10 font-weight-bold js-tab-title" href="{{route('allListProType', ['danhmuc'=>$type->cate_slug, 'loaidanhmuc'=>$type->slug])}}" title="{{languageName($type->name)}}">
                               <div>
                               </div>
                               {{languageName($type->name)}}
@@ -1004,12 +1004,84 @@
                   @if (count($cate->typeCate) > 0)
                   <div class="list_link_pr d-flex pt-2 pb-2">
                      @foreach ($cate->typeCate as $type)
-                        <a class=" box-shadow a-style bg-xanh border rounded-10 font-weight-bold js-tab-title" href="{{route('allListProType', ['danhmuc'=>$type->cate_slug, 'loaidanhmuc'=>$type->slug])}}" title="{{languageName($type->name)}}">
+                        <a class=" button-85 box-shadow a-style bg-xanh border rounded-10 font-weight-bold js-tab-title" href="{{route('allListProType', ['danhmuc'=>$type->cate_slug, 'loaidanhmuc'=>$type->slug])}}" title="{{languageName($type->name)}}">
                               <div>
                               </div>
                               {{languageName($type->name)}}
                         </a>
                      @endforeach
+                     <!-- HTML !-->
+
+
+<style>
+
+   .button-85 {
+     padding: 0.6em 2em;
+     border: none;
+     outline: none;
+     color: rgb(255, 255, 255);
+     background: #111;
+     cursor: pointer;
+     position: relative;
+     z-index: 0;
+     border-radius: 10px;
+     user-select: none;
+     -webkit-user-select: none;
+     touch-action: manipulation;
+   }
+   
+   .button-85:before {
+     content: "";
+     background: linear-gradient(
+       45deg,
+       #ff0000,
+       #ff7300,
+       #fffb00,
+       #48ff00,
+       #00ffd5,
+       #002bff,
+       #7a00ff,
+       #ff00c8,
+       #ff0000
+     );
+     position: absolute;
+     top: -2px;
+     left: -2px;
+     background-size: 400%;
+     z-index: -1;
+     filter: blur(5px);
+     -webkit-filter: blur(5px);
+     width: calc(100% + 4px);
+     height: calc(100% + 4px);
+     animation: glowing-button-85 20s linear infinite;
+     transition: opacity 0.3s ease-in-out;
+     border-radius: 10px;
+   }
+   
+   @keyframes glowing-button-85 {
+     0% {
+       background-position: 0 0;
+     }
+     50% {
+       background-position: 400% 0;
+     }
+     100% {
+       background-position: 0 0;
+     }
+   }
+   
+   .button-85:after {
+     z-index: -1;
+     content: "";
+     position: absolute;
+     width: 100%;
+     height: 100%;
+     background: #222;
+     left: 0;
+     top: 0;
+     border-radius: 10px;
+   }
+</style>
                   </div>
                   @endif
                   <h2 class="title text-uppercase font-weight-bold position-relative m-0">
